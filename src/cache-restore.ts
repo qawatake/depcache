f3f1cd37cf4618d7e7a07899f61bce9281279c61
qawatake <38106890+qawatake@users.noreply.github.com>
2024-08-10 20:04:59 +0900

    fmt

--- src/cache-restore.ts
+++ src/cache-restore.ts
@@ -1,59 +1,59 @@
 // some modifications were made to https://github.com/actions/setup-go/tree/v5.0.2/src
-import * as cache from '@actions/cache'
-import * as core from '@actions/core'
-import * as glob from '@actions/glob'
-import * as github from '@actions/github'
+import * as cache from "@actions/cache";
+import * as core from "@actions/core";
+import * as glob from "@actions/glob";
+import * as github from "@actions/github";
 
-import { State, Outputs } from './constants'
+import { State, Outputs } from "./constants";
 
 export const restoreCache = async (
-  jobId: string,
-  dependencyPath: string,
-  cachePaths: string[],
-  token: string
+	jobId: string,
+	dependencyPath: string,
+	cachePaths: string[],
+	token: string,
 ): Promise<void> => {
-  const fileHash = await glob.hashFiles(dependencyPath)
+	const fileHash = await glob.hashFiles(dependencyPath);
 
-  if (!fileHash) {
-    throw new Error(
-      'Some specified paths were not resolved, unable to cache dependencies.'
-    )
-  }
-  const oktokit = github.getOctokit(token)
-  const { data: workflowRun } = await oktokit.rest.actions.getWorkflowRun({
-    repo: github.context.repo.repo,
-    owner: github.context.repo.owner,
-    run_id: github.context.runId
-  })
-  const { data: workflow } = await oktokit.rest.actions.getWorkflow({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    workflow_id: workflowRun.workflow_id
-  })
-  const workflowPath = workflow.path
-    .replace(/^\.github\/workflows\//, '')
-    .replaceAll(',', '-')
-  const platform = process.env.RUNNER_OS
-  const linuxVersion =
-    process.env.RUNNER_OS === 'Linux' ? `${process.env.ImageOS}-` : ''
-  const cacheKeyPrefix = `depcache-${workflowPath}-${jobId}-${platform}-${linuxVersion}`
-  const primaryKey = `${cacheKeyPrefix}${fileHash}`
-  const secondaryKey = `${cacheKeyPrefix}`
-  core.debug(`primary key is ${primaryKey}`)
+	if (!fileHash) {
+		throw new Error(
+			"Some specified paths were not resolved, unable to cache dependencies.",
+		);
+	}
+	const oktokit = github.getOctokit(token);
+	const { data: workflowRun } = await oktokit.rest.actions.getWorkflowRun({
+		repo: github.context.repo.repo,
+		owner: github.context.repo.owner,
+		run_id: github.context.runId,
+	});
+	const { data: workflow } = await oktokit.rest.actions.getWorkflow({
+		owner: github.context.repo.owner,
+		repo: github.context.repo.repo,
+		workflow_id: workflowRun.workflow_id,
+	});
+	const workflowPath = workflow.path
+		.replace(/^\.github\/workflows\//, "")
+		.replaceAll(",", "-");
+	const platform = process.env.RUNNER_OS;
+	const linuxVersion =
+		process.env.RUNNER_OS === "Linux" ? `${process.env.ImageOS}-` : "";
+	const cacheKeyPrefix = `depcache-${workflowPath}-${jobId}-${platform}-${linuxVersion}`;
+	const primaryKey = `${cacheKeyPrefix}${fileHash}`;
+	const secondaryKey = `${cacheKeyPrefix}`;
+	core.debug(`primary key is ${primaryKey}`);
 
-  core.saveState(State.CachePrimaryKey, primaryKey)
+	core.saveState(State.CachePrimaryKey, primaryKey);
 
-  const cacheKey = await cache.restoreCache(cachePaths, primaryKey, [
-    secondaryKey
-  ])
-  core.setOutput(Outputs.CacheHit, Boolean(cacheKey))
+	const cacheKey = await cache.restoreCache(cachePaths, primaryKey, [
+		secondaryKey,
+	]);
+	core.setOutput(Outputs.CacheHit, Boolean(cacheKey));
 
-  if (!cacheKey) {
-    core.info(`Cache is not found`)
-    core.setOutput(Outputs.CacheHit, false)
-    return
-  }
+	if (!cacheKey) {
+		core.info(`Cache is not found`);
+		core.setOutput(Outputs.CacheHit, false);
+		return;
+	}
 
-  core.saveState(State.CacheMatchedKey, cacheKey)
-  core.info(`Cache restored from key: ${cacheKey}`)
-}
+	core.saveState(State.CacheMatchedKey, cacheKey);
+	core.info(`Cache restored from key: ${cacheKey}`);
+};
